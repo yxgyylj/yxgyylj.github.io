369c06120c82489653f8fb477941f4b4ff13702f
yxgyylj <yxgyylj@users.noreply.github.com>
2019-08-07 21:28:40 -0700

    Update main.js

--- 20190806/scripts/main.js
+++ 20190806/scripts/main.js
@@ -22,7 +22,7 @@ window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
     quiver = true,
     text = texts[0],
     textIndex = 0,
-    textSize = 55
+    textSize = Math.max(CANVASWIDTH,CANVASHEIGHT) / 10
 
   function draw () {
     ctx.clearRect(0, 0, CANVASWIDTH, CANVASHEIGHT)
